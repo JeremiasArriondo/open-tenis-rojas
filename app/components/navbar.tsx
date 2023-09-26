7eafde218744a280d8fac9aef18cfa7126932e2b
Jeremias Arriondo <jeremiasarriondo98@gmail.com>
2023-09-26 17:32:09 -0300

    feat: sticky navbar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -2,17 +2,21 @@ import Image from "next/image";
 
 export const Navbar = () => {
   return (
-    <header>
-      <nav className="h-16 flex items-center justify-center border-b border-gray-200 bg-[#f48633] text-white">
+    <header className="z-50 sticky top-0">
+      <nav className="h-16 px-8 flex items-center border-b border-gray-200 bg-white/80 backdrop-filter backdrop-blur-3xl backdrop-saturate-200">
         {/*<span className="ball">🎾</span>*/}
-        <Image
-          src="/images/logo.png"
-          alt="Open Rojas logo"
-          width={40}
-          height={40}
-        />
-        <span className="text-2xl font-bold">Open Rojas 2023</span>{" "}
-        <span className="ball">🎾</span>
+        <div className="flex items-center gap-2">
+          <Image
+            src="/images/logo.png"
+            alt="Open Rojas logo"
+            width={50}
+            height={50}
+          />
+          <span className="text-2xl font-extrabold bg-gradient-to-r from-amber-500 via-orange-600 to-yellow-500 bg-clip-text text-transparent">
+            Open Rojas 2023
+          </span>
+          <span className="ball">🎾</span>
+        </div>
       </nav>
     </header>
   );
